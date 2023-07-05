a715a5a4df143d96255c97dd423a754f2c7eaa04
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-07-05 21:25:42 +0200

    Added sitemap support

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,9 @@
 import { defineConfig } from "vitepress";
+import { createWriteStream } from "node:fs";
+import { resolve } from "node:path";
+import { SitemapStream } from "sitemap";
+
+const links = [];
 
 const sidebarGuide = () => [
   {
@@ -389,4 +394,23 @@ export default defineConfig({
     //   `,
     // ],
   ],
+
+  transformHtml: (_, id, { pageData }) => {
+    if (!/[\\/]404\.html$/.test(id))
+      links.push({
+        // you might need to change this if not using clean urls mode
+        url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, "$2"),
+        lastmod: pageData.lastUpdated,
+      });
+  },
+
+  buildEnd: ({ outDir }) => {
+    const sitemap = new SitemapStream({
+      hostname: "https://axe-api.com/",
+    });
+    const writeStream = createWriteStream(resolve(outDir, "sitemap.xml"));
+    sitemap.pipe(writeStream);
+    links.forEach((link) => sitemap.write(link));
+    sitemap.end();
+  },
 });

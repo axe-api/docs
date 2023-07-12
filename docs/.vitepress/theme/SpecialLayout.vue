811044f5234ed920bbb844b528d42ebc3fdccd3c
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-07-12 18:52:01 +0200

    Added video introduction

--- docs/.vitepress/theme/SpecialLayout.vue
+++ docs/.vitepress/theme/SpecialLayout.vue
@@ -5,15 +5,16 @@
       left: 0px;
       right: 0px;
       margin: auto;
-      margin-bottom: 50px;
+      margin-top: 100px;
+      margin-bottom: 100px;
       display: flex;
       justify-content: center;
     "
   >
     <iframe
-      width="1000"
-      height="572"
-      src="https://www.youtube.com/embed/C50CpW4x82M"
+      width="1152"
+      height="658"
+      src="https://www.youtube.com/embed/3p4jggsNrJA"
       title="Axe API Introduction"
       frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"

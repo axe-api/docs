2f3ab50649d50c11a289a4f82eb30cac6917bc9b
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-07-12 16:19:50 +0200

    Added video.

---
+++ docs/.vitepress/theme/SpecialLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    style="
+      width: 1152px;
+      left: 0px;
+      right: 0px;
+      margin: auto;
+      margin-bottom: 50px;
+      display: flex;
+      justify-content: center;
+    "
+  >
+    <iframe
+      width="1000"
+      height="572"
+      src="https://www.youtube.com/embed/C50CpW4x82M"
+      title="Axe API Introduction"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      style="padding: 10px"
+    ></iframe>
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,14 @@
+import { h } from "vue";
 import Theme from "vitepress/theme";
+import SpecialLayout from "./SpecialLayout.vue";
 
 import "./styles/vars.css";
 
 export default {
   ...Theme,
+  Layout() {
+    return h(Theme.Layout, null, {
+      "home-hero-after": () => h(SpecialLayout),
+    });
+  },
 };

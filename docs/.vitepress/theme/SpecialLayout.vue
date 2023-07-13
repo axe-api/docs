2d2e3c153494f8b8ea44ac515d5ffbed38c889f3
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-07-13 13:36:23 +0200

    Fixed design issues

--- docs/.vitepress/theme/SpecialLayout.vue
+++ docs/.vitepress/theme/SpecialLayout.vue
@@ -1,11 +1,11 @@
 <style>
-.container {
+.videoContainer {
   width: 1152px;
   height: 658px;
   left: 0px;
   right: 0px;
   margin: auto;
-  margin-top: -100px;
+  margin-top: 100px;
   margin-bottom: 100px;
   display: flex;
   justify-content: center;
@@ -13,14 +13,14 @@
   @media only screen and (max-width: 768px) {
     width: 90vw;
     height: 55vw;
-    margin-top: 150px;
-    margin-bottom: 50px;
+    margin-top: 100px;
+    margin-bottom: 100px;
   }
 }
 </style>
 
 <template>
-  <div class="container">
+  <div class="videoContainer">
     <iframe
       width="100%"
       height="100%"

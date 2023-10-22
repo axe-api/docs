1c09a836e5c7fabeadb55a3d0d7481599e1a43c3
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2023-10-22 15:34:16 +0200

    Added auto-caching upgrade

--- docs/.vitepress/cache/deps/vue.js
+++ docs/.vitepress/cache/deps/vue.js
@@ -153,7 +153,7 @@ import {
   withMemo,
   withModifiers,
   withScopeId
-} from "./chunk-V634PGSD.js";
+} from "./chunk-TQFIIZRC.js";
 export {
   BaseTransition,
   BaseTransitionPropsValidators,
